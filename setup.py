3fc46cdbcb2c34fbab643f1b7312b673219dc082
2INFINITY <48837503+2infinity-gh@users.noreply.github.com>
2019-05-09 16:26:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, Extension
 
-lyra2z_hash_module = Extension('lyra2z_hash',
+lyra2z_hash_module = Extension('lyra2zc_hash',
                                sources = [
 										  'lyra2zmodule.c',
                                           'lyra2z.c',
@@ -9,11 +9,11 @@ lyra2z_hash_module = Extension('lyra2z_hash',
 										  'blake.c'],
                                include_dirs=['.'])
 
-setup (name = 'lyra2z_hash',
+setup (name = 'lyra2zc_hash',
        version = '0.1.0',
        author_email = 'devwarrior.zcoin@gmail.com',
        author = 'devwarrior',
-       url = 'https://github.com/devwarrior777/lyra2z-py',
-       description = 'Bindings for Lyra2Z proof of work used by Zcoin',
+       url = 'https://github.com/2infinity-gh/lyra2zc-py',
+       description = 'Bindings for Lyra2CZ proof of work used by BitcoinCZ',
        ext_modules = [lyra2z_hash_module])
 
